9013b072ea6aed7842d2e31aa8de94cff2fbbc14
Dimitry Nasrtdinov <75541143+HolloWiccup@users.noreply.github.com>
2023-09-06 09:47:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,18 @@ const TelegramBot = require('node-telegram-bot-api');
 // Ваш токен бота
 // const TOKEN = '6205695064:AAFj3u_lJdHsWC3cW7tl9z9JlzcxCcpS-b4';
 const TOKEN = '6545611484:AAFZA1Rgp6SPSouzYBdgD8b54Zsm2kjHPs0';
-const MESSAGE_FOR_USER = 'Вашим вопросом уже занимаются';
 
-const INTRO_PHRASE = 'вопрос знатокам';
+const MESSAGE_FOR_USER = 'Вашим вопросом уже занимаются';
+const SORRY_MESSAGE =
+  'Извините, но я такой фразы больше не знаю, зато умею реагировать на "вопрос ментору"';
+const regexMentor = /в[ао]*пр[ао]*[сш]\s*м[еэ]*нт[ао]*р[уам ]*/gi;
+const regexOld = /в[ао]*пр[ао]*[сш]\s*зн[а]*т[ао]*к[уиам ]*/gi;
 
 const SELECT = ' ✅';
 
 const MY_ID = 819208805;
 const ARTEM_ID = 992108033;
 const CHANNEL_ID = -905881279;
-
-const regex = /в[ао]*пр[о]*[сш]\s*м[е]*нт[о]*р[уам ]*/ig;
 
 const button1 = {
   text: 'Взял',
@@ -34,11 +35,16 @@ const bot = new TelegramBot(TOKEN, { polling: true });
 
 // Обработчик сообщений
 bot.on('message', (msg) => {
-  if (msg.chat.id === CHANNEL_ID) return;
-
+  if (msg.chat.id === CHANNEL_ID || msg.from.is_bot) return;
   const messageText = msg.text;
 
-  if (!regex.test(messageText)) return;
+  const option = regexMentor.test(messageText)
+    ? 1
+    : regexOld.test(messageText)
+    ? 2
+    : 0;
+
+  if (!option) return;
 
   const id = msg.chat.id.toString();
   const isForum = Boolean(msg.chat?.is_forum);
@@ -57,13 +63,20 @@ bot.on('message', (msg) => {
     messageLink += `${threadId}/`;
   }
 
-  messageLink += messageId
+  messageLink += messageId;
 
   message += ` ${messageLink}`;
-  const second_message = `Поставленный вопрос: ${messageText.replace(regex, '')}`;
+  const second_message = `Поставленный вопрос: ${messageText.replace(
+    regexMentor,
+    ''
+  )}`;
+
+  if (option === 2) {
+    return bot.sendMessage(id, SORRY_MESSAGE, { message_thread_id: threadId });
+  }
 
   sendToChannel(`${message}\n${second_message}`);
-  bot.sendMessage(id, MESSAGE_FOR_USER, { message_thread_id: threadId })
+  bot.sendMessage(id, MESSAGE_FOR_USER, { message_thread_id: threadId });
 });
 
 async function sendToChannel(message) {
@@ -79,9 +92,9 @@ bot.on('callback_query', (callbackQuery) => {
   const chatId = message.chat.id;
   const data = callbackQuery.data;
   const newKeyboard = createNewButtons(data);
-  // const postscript = getAssistantName(message.text, callbackQuery);
+  const postscript = getAssistantName(message.text, callbackQuery);
 
-  bot.editMessageText(message.text, {
+  bot.editMessageText(message.text + postscript, {
     chat_id: chatId,
     message_id: message.message_id,
     reply_markup: newKeyboard,
@@ -89,21 +102,25 @@ bot.on('callback_query', (callbackQuery) => {
 });
 
 const createNewButtons = (callback_data) => {
-  const newButtons = buttons.map((button) =>
-    button.callback_data === callback_data
-      ? { ...button, text: button.text + SELECT }
-      : { ...button }
-  );
+  // const newButtons = buttons.map((button) =>
+  //   button.callback_data === callback_data
+  //     ? { ...button, text: button.text + SELECT }
+  //     : { ...button }
+  // );
+  // return {
+  //   inline_keyboard: [newButtons],
+  // };
+  const newButtons = callback_data === 'button1' ? [button2] : [];
   return {
     inline_keyboard: [newButtons],
   };
 };
 
-// const getAssistantName = (message, callback_data) => {
+const getAssistantName = (message, callback_data) => {
 
-//   const data = callback_data.data;
-//   const user = callback_data.from.username;
+  const data = callback_data.data;
+  const user = callback_data.from.username;
 
-//   const action = buttons.find((btn) => btn.callback_data === data).text;
-//   return `\n@${user} ${action}`;
-// };
+  const action = buttons.find((btn) => btn.callback_data === data).text;
+  return `\n@${user} ${action}`;
+};
